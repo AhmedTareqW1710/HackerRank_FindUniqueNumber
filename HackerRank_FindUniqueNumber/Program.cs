d0a628498105e4a75d8a4bd2444af765a86eb214
aebrahim <aebrahim@THQ1LTAEBRAHIM>
2019-09-15 23:08:25 +0300

    test passed

--- HackerRank_FindUniqueNumber/Program.cs
+++ HackerRank_FindUniqueNumber/Program.cs
@@ -1,15 +1,60 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
 using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
 using System.Text;
-using System.Threading.Tasks;
+using System;
 
-namespace HackerRank_FindUniqueNumber
+class Solution
 {
-    class Program
+
+    // Complete the sockMerchant function below.
+    static int FindUniqueNumber(int[] ar)
     {
-        static void Main(string[] args)
+        int result = -1;
+       
+        for (int i = 0; i < ar.Length; i++)
         {
+            result = ar[i];
+
+            for (int j = 0; j < ar.Length; j++)
+            {
+                if (i != j)
+                {
+                    if (ar[j] == result)
+                    {
+                        result = -1;
+                        break;
+                    }
+                }
+            }
+
+            if (result != -1)
+            {
+                break;
+            }
         }
+
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+        int result = FindUniqueNumber(ar);
+
+        Console.WriteLine(result);
+        Console.ReadKey();
     }
 }
